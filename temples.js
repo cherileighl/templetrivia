a61e323a30816c0fc6609b4fc661d074cd95d459
Cherileigh <56326118+cherileighl@users.noreply.github.com>
2020-03-12 16:56:28 -0600

    First commit

---
+++ temples.js
@@ -0,0 +1,262 @@
+/* 
+    temples:
+    1st = Location
+    2nd = File name (image)
+    3rd = Status
+*/
+
+let templeArray = [
+    [
+        "Aba Nigeria",
+        "abanigeria",
+        "Open"
+    ],
+    [
+        "Abidjan Ivory Coast",
+        "abidjanivorycoast",
+        "Construction"
+    ],
+    [
+        "Accra Ghana",
+        "accraghana",
+        "Open"
+    ],
+    [
+        "Adelaide Australia",
+        "adelaideaustralia",
+        "Open"
+    ],
+    [
+        "Albuquerque New Mexico",
+        "albuquerquenewmexico",
+        "Open"
+    ],
+    [
+        "Anchorage Alaska",
+        "anchoragealaska",
+        "Open"
+    ],
+    [
+        "Apia Samoa",
+        "apiasamoa",
+        "Open"
+    ],
+    [
+        "Arequipa Peru",
+        "arequipaperu",
+        "Open"
+    ],
+    [
+        "Asunción Paraguay",
+        "asunciónparaguay",
+        "Open"
+    ],
+    [
+        "Atlanta Georgia",
+        "atlantageorgia",
+        "Open"
+    ],
+    [
+        "Bangkok Thailand",
+        "bangkokthailand",
+        "Construction"
+    ],
+    [
+        "Barranquilla Colombia",
+        "barranquillacolombia",
+        "Open"
+    ],
+    [
+        "Baton Rouge Louisiana",
+        "batonrougelouisiana",
+        "Open"
+    ],
+    [
+        "Belém Brazil",
+        "belémbrazil",
+        "Construction"
+    ],
+    [
+        "Bern Switzerland",
+        "bernswitzerland",
+        "Open"
+    ],
+    [
+        "Billings Montana",
+        "billingsmontana",
+        "Open"
+    ],
+    [
+        "Birmingham Alabama",
+        "birminghamalabama",
+        "Open"
+    ],
+    [
+        "Bismarck North Dakota",
+        "bismarcknorthdakota",
+        "Open"
+    ],
+    [
+        "Bogotá Colombia",
+        "bogotácolombia",
+        "Open"
+    ],
+    [
+        "Boise Idaho",
+        "boiseidaho",
+        "Open"
+    ],
+    [
+        "Boston Massachusetts",
+        "bostonmassachusetts",
+        "Open"
+    ],
+    [
+        "Bountiful Utah",
+        "bountifulutah",
+        "Open"
+    ],
+    [
+        "Brigham City Utah",
+        "brighamcityutah",
+        "Open"
+    ],
+    [
+
+        "Brisbane Australia",
+        "brisbaneaustralia",
+        "Open"
+    ],
+    [
+        "Buenos Aires Argentina",
+        "buenosairesargentina",
+        "Open"
+    ],
+    [
+        "Calgary Alberta",
+        "calgaryalberta",
+        "Open"
+    ],
+    [
+        "Campinas Brazil",
+        "campinasbrazil",
+        "Open"
+    ],
+    [
+        "Caracas Venezuela",
+        "caracasvenezuela",
+        "Open"
+    ],
+    [
+        "Cardston Alberta",
+        "cardstonalberta",
+        "Open"
+    ],
+    [
+        "Cebu City Philippines",
+        "cebucityphilippines",
+        "Open"
+    ],
+    [
+        "Cedar City Utah",
+        "cedarcityutah",
+        "Open"
+    ],
+    [
+        "Chicago Illinois",
+        "chicagoillinois",
+        "Open"
+    ],
+    [
+        "Ciudad Juárez Mexico",
+        "ciudadjuárezmexico",
+        "Open"
+    ],
+    [
+        "Cochabamba Bolivia",
+        "cochabambabolivia",
+        "Open"
+    ],
+    [
+        "Colonia Juárez Chihuahua Mexico",
+        "coloniajuárezchihuahuamexico",
+        "Open"
+    ],
+    [
+        "Columbia River Washington",
+        "columbiariverwashington",
+        "Open"
+    ],
+    [
+        "Columbia South Carolina",
+        "columbiasouthcarolina",
+        "Open"
+    ],
+    [
+        "Columbus Ohio",
+        "columbusohio",
+        "Open"
+    ],
+    [
+        "Concepción Chile",
+        "concepciónchile",
+        "Open"
+    ],
+    [
+        "Copenhagen Denmark",
+        "copenhagendenmark",
+        "Open"
+    ],
+    [
+        "Córdoba Argentina",
+        "córdobaargentina",
+        "Open"
+    ],
+    [
+        "Curitiba Brazil",
+        "curitibabrazil",
+        "Open"
+    ],
+    [
+        "Dallas Texas",
+        "dallastexas",
+        "Open"
+    ],
+    [
+        "Denver Colorado",
+        "denvercolorado",
+        "Open"
+    ],
+    [
+        "Detroit Michigan",
+        "detroitmichigan",
+        "Open"
+    ],
+    [
+        "Draper Utah",
+        "draperutah",
+        "Open"
+    ],
+    [
+        "Durban South Africa",
+        "durbansouthafrica",
+        "Open"
+    ],
+    [
+        "Edmonton Alberta",
+        "edmontonalberta",
+        "Open"
+    ]
+
+
+
+
+
+];
+
+
+
+
+
+
+
